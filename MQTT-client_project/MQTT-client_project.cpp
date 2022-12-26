c686623a9f8ef8ec6d4d556bd73fac5985c718ea
Mateusz Zuber <mateusz.zuber@siroccomobile.com>
2022-12-26 22:43:31 +0100

    Commit - etap 3 --> gotowy projekt

--- MQTT-client_project/MQTT-client_project.cpp
+++ MQTT-client_project/MQTT-client_project.cpp
@@ -1,13 +1,24 @@
 ﻿#define CURL_STATICLIB
 #include <iostream>
 #include <ostream>
-//#include <stdlib.h>
+//include <stdlib.h>
 #include <cstdlib>
 #include <mosquitto.h>
 #include "curl/curl.h"
 #include <string>
 #include <wincrypt.h>
 #include <nlohmann/json.hpp>
+#ifdef _DEBUG
+#pragma comment (lib, "curl/libcurl_a_debug.lib")
+#else
+#pragma comment (lib, "curl/libcurl_a.lib")
+#endif
+#pragma comment (lib, "Crypt32.lib")
+#pragma comment (lib, "Normaliz.lib")
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Wldap32.lib")
+#pragma comment (lib, "advapi32.lib")
+using json = nlohmann::json;
 
 size_t weather_write_data(void* ptr, size_t size, size_t nmemb, void* str);
 size_t curl_callback(void* contents, size_t size, size_t nmemb, void* userp);
@@ -18,10 +29,42 @@ int main()
 	CURL* curl;
 	CURLcode res;
 	curl_global_init(CURL_GLOBAL_DEFAULT);
-	string const url = "https://danepubliczne.imgw.pl/api/data/synop/id/12205";
-	//json weather_data;
+	string const url = "https://danepubliczne.imgw.pl/api/data/synop/id/12560";
+	json weather_data;
 	string dataString;
-	string data; //zmienna testowa - GIT
+	string data; 
+
+	curl = curl_easy_init();
+	if (!curl) 
+	{
+		cout << "Error: cannot initialize CURL." << endl;
+		return 1;
+	}
+	res = curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+	if (res != CURLE_OK)
+	{
+		cout << "Cannot set curl url." << endl;
+	}
+	res = curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+	if (res != CURLE_OK) 
+	{
+		cout << "Cannot set curl follow location flag." << endl;
+	}
+	res = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_callback); // weather_write_data); // --> string > JSON
+	if (res != CURLE_OK) 
+	{
+		cout << "Cannot set the weather write function." << endl;
+	}
+	res = curl_easy_setopt(curl, CURLOPT_WRITEDATA, &weather_data); // &dataString); // --> string > JSON
+	if (res != CURLE_OK) 
+	{
+		cout << "Cannot set the curl write data." << endl;
+	}
+	res = curl_easy_perform(curl);
+	if (res != CURLE_OK) 
+	{
+		cout << "Cannot perform curl." << endl;
+	}
 
 	//Wysyłanie na serwer mosquitto
 	int rc;
@@ -29,11 +72,9 @@ int main()
 	const int MQTT_PORT = 1883;
 	mosquitto_lib_init();
 	struct mosquitto* mosq;
-
 	string topic = "topic";
-
+	dataString = weather_data.dump();
 	mosq = mosquitto_new("Client", true, NULL);
-
 	rc = mosquitto_connect(mosq, MQTT_SERVER.c_str(), MQTT_PORT, 60);
 	if (rc != 0) {
 		cout << "Client couldn't connect to broker" << endl;
@@ -43,9 +84,8 @@ int main()
 		cout << "Connected with broker!" << endl;
 	}
 
-
 	if (mosquitto_publish(mosq, NULL, topic.c_str(), dataString.size(), dataString.c_str(), 0, false) == MOSQ_ERR_SUCCESS) {
-		cout << "send" << endl;
+		cout << "MQTT: Published correctly" << endl;
 	}
 	else
 	{
@@ -56,5 +96,37 @@ int main()
 	mosquitto_destroy(mosq);
 
 	return 1;
-
 }
+
+size_t weather_write_data(void* ptr, size_t size, size_t nmemb, void* str) {
+	string* s = static_cast<string*>(str);
+	copy((char*)ptr, (char*)ptr + (size + nmemb), back_inserter(*s));
+	return size * nmemb;
+}
+size_t curl_callback(void* contents, size_t size, size_t nmemb, void* userp) {
+	json* obj = (json*)userp;
+	size_t realsize = size * nmemb;
+	string data((char*)contents, realsize);
+
+	try {
+		json new_data = json::parse(data);
+		*obj = new_data;
+	}
+	catch (const json::parse_error& e) {
+		cerr << "Error parsing JSON data: " << e.what() << endl;
+	}
+	return realsize;
+}
+/*
+JSON - preferowana struktura danych:
+id_stacji:
+stacja:
+data_pomiaru:
+godzina_pomiaru:
+temperatura:
+predkosc_wiatru:
+kierunek_wiatru:
+wilgotnosc_wzgledna:
+suma_opadu:
+cisnienie:
+*/
